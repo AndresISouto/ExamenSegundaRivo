b5ce0cd2619b9e4b0b03e4216e6043f91d2ab448
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-13 12:18:26 +0100

    native query

--- src/main/java/com/examen/examen2rico/repositories/PersonaRepository.java
+++ src/main/java/com/examen/examen2rico/repositories/PersonaRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.examen.examen2rico.models.Persona;
@@ -13,7 +14,7 @@ public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
   Optional<Persona> findByDni(String dni);
 
-  // @Query("SELECT SUM(p.vl.salarioBrutoAnual ) FROM Persona p LEFT JOIN FETCH
-  // VidaLaborar vl GROUP BY p.id WHERE p.id=:id")
+  @Query("SELECT SUM(p.vl.salarioBrutoAnual ) FROM Persona p LEFT JOIN FETCH VidaLaborar vl GROUP BY p.id WHERE p.id=:id")
+  Double allSalary(@Param("id") Long id);
 
 }
