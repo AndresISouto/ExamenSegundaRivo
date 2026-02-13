1cc956ee07d90ae1691e3a1a391fb7099e63ef8e
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-13 10:18:37 +0100

    initial

---
+++ src/main/java/com/examen/examen2rico/Examen2ricoApplication.java
@@ -0,0 +1,13 @@
+package com.examen.examen2rico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2ricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2ricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/examen2rico/Examen2ricoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.examen2rico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2ricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
